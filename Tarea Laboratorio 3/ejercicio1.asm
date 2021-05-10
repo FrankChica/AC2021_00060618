de83a36f3038452a2d9805ceced1a7dfbcc1e503
Francisco Rodriguez <00060618@uca.edu.sv>
2021-05-10 00:43:20 -0600

    tarea lab3

--- Tarea Laboratorio 3/ejercicio1.asm
+++ Tarea Laboratorio 3/ejercicio1.asm
@@ -3,21 +3,30 @@
 	section .text
 
 	XOR AX, AX
-	XOR BX, BX
-	jmp proc
-
-	proc:
-	ADD AX, 0D ;sumas
-	ADD AX, 0D
-	ADD AX, 0D
-	ADD AX, 6D
-	ADD AX, 0D
-	ADD AX, 6D
-	ADD Ax, 1D
-	ADD Ax, 8D
+	XOr Bx, BX
+	MOV byte[200H], 0d
+	MOV byte[201H], 0d
+	MOV byte[202H], 0d
+	MOV byte[203H], 6d
+	MOV byte[204H], 0d
+	MOV byte[205H], 6d
+	MOV byte[206H], 1d
+	MOV byte[207H], 8d
+	MOV AL, 0d
+	MOV DI, 0d
 	MOV BX, 8D
+	jmp iterar
+	
+    iterar:
+	CMP DI, 8d
+	JE exit
+    ADD AL, [200h + DI]
+	add DI, 1d
+
+	jmp iterar
+    exit:
+
     DIV BX ;Division
 	MOV byte[20AH], AL
-	exit:
         
 	int 20H
